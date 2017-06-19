48665512d5d97698a28d13e73784c832b6596c4c
sun0110 <balancebug@163.com>
2017-06-19 20:41:53 +0800

    Revert "git revert test---1-commit"
    This reverts commit 108e70629a20bdbdc1fab02dd8bede37a28f6f93.

--- src/main/java/com/sun/controller/Hello.java
+++ src/main/java/com/sun/controller/Hello.java
@@ -1,28 +1,26 @@
 package com.sun.controller;
 
-import com.sun.service.AService;
-import com.sun.service.BService;
-import com.sun.util.ApplicationContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import com.sun.service.AService;
+import com.sun.service.BService;
+import com.sun.util.ApplicationContextHolder;
 
 @RestController
 public class Hello {
 
-
-	@Resource(name="aService")
+	/*@Resource(name="aServiceDubbo")
 	private AService aServiceDubbo;
 	
-	@Resource(name="bService")
-	private BService bServiceDubbo;
+	@Resource(name="bServiceDubbo")
+	private BService bServiceDubbo;*/
 	
     @RequestMapping("/")
     public String greeting() {
-    	AService as = (AService) ApplicationContextHolder.getBean("aService");
+    	AService as = (AService) ApplicationContextHolder.getBean("aServiceDubbo");
     	as.sayHi("sun");
-    	BService bs = (BService) ApplicationContextHolder.getBean("bService");
+    	BService bs = (BService) ApplicationContextHolder.getBean("bServiceDubbo");
     	bs.sayHi("sun");
         return "";
     }
